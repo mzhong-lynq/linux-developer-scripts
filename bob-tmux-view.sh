0e4c58ab00e3afd63772a49668c281079fb61786
Matthew Zhong <matthew.zhong@lynqnetworks.com>
2025-03-21 01:44:09 -0400

    Modify tmux viewer to use udev rule generator

--- bob-tmux-view.sh
+++ bob-tmux-view.sh
@@ -2,20 +2,16 @@
 
 set -euo pipefail
 
-if [[ "$#" != "2" ]]; then
-    SILICON_LABS_IDS=$(ls /dev/serial/by-id/usb-Silicon_Labs_CP2105_Dual_USB_to_UART_Bridge_Controller_*if00* | sort | uniq | sed -E 's/.*([0-9A-F]{8}).*/\1/' | tr '\n' ' ')
-    ZEPHYR_IDS=$(ls /dev/serial/by-id/usb-ZEPHYR_LNQ3195_* | sed -E 's/.*([0-9A-F]{16}).*/\1/' | sort | uniq | tr '\n' ' ')
-
-    echo "Usage: $0 <zephyr-id> <silicon-labs-id>"
-    echo "    Zephyr IDs: ${ZEPHYR_IDS}"
-    echo "    Silicon Labs IDs: ${SILICON_LABS_IDS}"
+if [[ "$#" != "1" ]]; then
+    echo "Usage: $0 <device-idx>"
+    echo "    Choices: $(ls /dev/rtHST*-0)"
     exit 1
 fi
 
-ZEPHYR_ID=$1
-SILICON_LABS_ID=$2
+device_idx=$1
 
-tmux new-window "tio -b 115200 /dev/serial/by-id/usb-ZEPHYR_LNQ3195_${ZEPHYR_ID}-if00"
-tmux split-window -h "tio -b 115200 /dev/serial/by-id/usb-Silicon_Labs_CP2105_Dual_USB_to_UART_Bridge_Controller_${SILICON_LABS_ID}-if00-port0"
-tmux split-window -h "tio -b 115200 /dev/serial/by-id/usb-Silicon_Labs_CP2105_Dual_USB_to_UART_Bridge_Controller_${SILICON_LABS_ID}-if01-port0"
+tmux new-window "minicom -b 115200 -8 -D /dev/rtHST${device_idx}-0"
+tmux split-window -h "minicom -b 115200 -8 -D /dev/rtDBG${device_idx}-0"
+tmux split-window -h "minicom -b 115200 -8 -D /dev/rtDBG${device_idx}-1"
+tmux rename-window "RT${device_idx} Serial"
 tmux select-layout even-horizontal
